8b7821f378f9b6555c509637c177a767ccdcf6d2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-06-30 13:42:19 -0300

    chore-class Vehicle-increments the class and implements it methods.

--- src/log/trybe/Vehicle.java
+++ src/log/trybe/Vehicle.java
@@ -1,13 +1,56 @@
 package log.trybe;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public abstract class Vehicle {
-  String plate;
-  int manufactureYear;
-  String fuelType;
-  List refuellings;
+  private String plate;
+  private int manufactureYear;
+  private String fuelType;
+  private ArrayList<String> refuellings;
 
-  public abstract void toFuel(String place, String driver, double price);
-  public abstract void refuellingsList();
+  public Vehicle(String plate, int manufactureYear) {
+    this.plate = plate;
+    this.manufactureYear = manufactureYear;
+    this.refuellings = new ArrayList<String>();
+  }
+
+  public String getPlate() {
+    return plate;
+  }
+
+  public int getManufactureYear() {
+    return manufactureYear;
+  }
+
+  public String getFuelType() {
+    return fuelType;
+  }
+
+  public ArrayList<String> getRefuellings() {
+    return refuellings;
+  }
+
+  public void setFuelType(String fuelType) {
+    this.fuelType = fuelType;
+  }
+
+  public void setRefuellings(ArrayList refuellings) {
+    this.refuellings = refuellings;
+  }
+
+  public void toFuel(String place, String driver, double price){
+    this.getRefuellings().add(
+        "Local: " + place + "\n"
+            + "Motorista: " + driver + "\n"
+            + "Valor: " + price
+    );
+  };
+  public void refuellingsList() {
+    System.out.println("+-- Lista de Abastecimentos para o veículo: " + this.getPlate() + "--+");
+    for (String refuelling : this.getRefuellings()) {
+      System.out.println("|" + refuelling + "\t|");
+    }
+
+    System.out.println("+--------------------------------- Fim ---------------------------------+");
+  };
 }
