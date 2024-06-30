8f1eb3086e9ed3eddeddaa8cf69780c6afce5c70
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-06-30 13:36:09 -0300

    chore-class Car-increments the method refuellingsList() and set the constructor.

--- src/log/trybe/Car.java
+++ src/log/trybe/Car.java
@@ -2,13 +2,30 @@ package log.trybe;
 
 public class Car extends Vehicle{
 
+  public Car(String plate, int manufactureYear) {
+    super(plate, manufactureYear);
+  }
+
   @Override
   public void toFuel(String place, String driver, double price) {
-    System.out.println("Abastecendo carro...");
+    System.out.println(
+        "Abastecendo carro: \n"
+        + " Placa: " + this.getPlate()
+        + " Local: " + place
+        + " Motorista: " + driver
+        + " Valor: " + price
+    );
+
+    super.toFuel(place, driver, price);
   }
 
   @Override
   public void refuellingsList() {
-    System.out.println("Registrando na lista de abastecimentos do carro...");
+    System.out.println("+-- Lista de Abastecimentos para o carro: " + this.getPlate() + "--+");
+    for (String refuelling : this.getRefuellings()) {
+      System.out.println("|" + refuelling + "\t|");
+    }
+
+    System.out.println("+--------------------------------- Fim ---------------------------------+");
   }
 }
