4d6d13a83a993706f6be994ac6c006d189632815
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-06-30 13:37:26 -0300

    chore-class Motorcycle-increments the methods refuellingsList() and toFuel() and set the constructor.

--- src/log/trybe/Motorcycle.java
+++ src/log/trybe/Motorcycle.java
@@ -1,13 +1,26 @@
 package log.trybe;
 
 public class Motorcycle extends Vehicle{
+
+  public Motorcycle(String plate, int manufactureYear) {
+    super(plate, manufactureYear);
+  }
+
   @Override
   public void toFuel(String place, String driver, double price) {
-    System.out.println("Abastecendo motocicleta...");
+    System.out.println(
+        "Abastecendo motocicleta:\n"
+            + " Placa: " + this.getPlate()
+            + " Local: " + place
+            + " Motorista: " + driver
+            + " Valor: " + price
+    );
+    super.toFuel(place, driver, price);
   }
 
   @Override
   public void refuellingsList() {
-    System.out.println("Registrando na lista de abastecimento de motocicleta...");
+    System.out.println("Lista de abastecimentos de motocicleta:\n");
+    super.refuellingsList();
   }
 }
